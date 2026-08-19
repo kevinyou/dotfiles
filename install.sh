065f838c1c53aace19e9e8a4d1b853186674947d
Kevin You <kevinyou@keviny-kjvn22k632.ht.home>
2026-08-19 13:48:10 -0500

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+ln -sf ~/dotfiles/.vimrc ~
+ln -sf ~/dotfiles/.vim ~
+ln -sf ~/dotfiles/.tmux.conf ~
+ln -sf ~/.zshrc ~/.zprofile
+ln -sf ~/dotfiles/.aerospace.toml ~
+
+# For Vanta
+/workspaces/obsidian/build-tools/vsx.sh scripts/marketplace-cli/src/cli.ts enable vanta-agent-developer
